eeea51fe197aafb25672e69eeabbde9ad697ef17
Aftab Hussain Miranda <wafto.mx@gmail.com>
2017-10-02 17:52:06 -0500

    Bootstrap controller and routes

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -4,7 +4,62 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+/**
+ * En esta clase deben implementar los metodos vacios de acuerdo a lo
+ * previamente estudiado acerca de RESTFul. Recuerda que DEBEN validar los datos
+ * de entrada y de regresar lo que les pide el método correctamente.
+ *
+ * Class TodoController
+ * @package App\Http\Controllers
+ */
 class TodoController extends Controller
 {
-    //
+    /**
+     * Este método del controlador regresa el listado del todos de la app
+     * en un response del tipo json.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        // TODO
+    }
+
+    /**
+     * Este método del controlador debe crear un nuevo registro todo
+     * y al final regresa el registro creado en un response del tipo
+     * json.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(Request $request)
+    {
+        // TODO
+    }
+
+    /**
+     * Modifica el item todo con el $id correspondiente
+     * y regresa el mismo item modificado.
+     *
+     * @param integer $id
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update($id, Request $request)
+    {
+        // TODO
+    }
+
+    /**
+     * Elimina el registro y devuelve un response 200 en caso de exito o un 404
+     * en caso de fallo pero igual en tipo json.
+     *
+     * @param integer $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function delete($id)
+    {
+        // TODO
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('todos', 'TodoController@index');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('todos', 'TodoController@store');
+
+Route::put('todos/{id}', 'TodoController@update');
+
+Route::delete('todos/{id}', 'TodoController@delete');
